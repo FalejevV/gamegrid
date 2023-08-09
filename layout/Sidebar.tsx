2ead2e9f7137bcd2436954395b3fa8f70284c74c
FalejevV <v.o.v.a@hotmail.com>
2023-08-09 18:54:33 +0300

    build css problem fix attempt

--- layout/Sidebar.tsx
+++ layout/Sidebar.tsx
@@ -8,8 +8,9 @@ export default function Sidebar(){
     const windowSitebarSelector = useAppSelector((state:RootState) => state.window.sidebarHovered);
 
     return(
-        <div className={`hidden w-[90px] h-screen max-h-screen bg-[#0D0306] fixed left-0 top-0 md:flex pt-[100px] transition-all duration-300
+        <div className={`hidden h-screen max-h-screen bg-[#0D0306] fixed left-0 top-0 md:flex pt-[100px] transition-all duration-300
         ${windowSitebarSelector && "w-[200px]"}
+        ${!windowSitebarSelector && "w-[90px]"}
         `}
         onMouseEnter={() => dispatch(toggleSidebarHover(true))}  onMouseLeave={() => dispatch(toggleSidebarHover(false))} 
             
