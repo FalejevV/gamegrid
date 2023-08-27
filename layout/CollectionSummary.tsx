54e82bc31a5582ce62d02a748159ff67282d1f97
FalejevV <v.o.v.a@hotmail.com>
2023-08-27 11:03:20 +0300

    Hardcoded dashboard grid

--- layout/CollectionSummary.tsx
+++ layout/CollectionSummary.tsx
@@ -1,4 +1,5 @@
 import { GameReviewData } from "@/interface"
+import games from "@/store/features/games"
 
 
 
@@ -16,8 +17,51 @@ export default function CollectionSummary(props: {
     )
 
     return (
-        <section className="w-full h-[400px] bg-[#ffffff18] flex items-center justify-center textcol-main text-[30px]">
-            <p>GAMES COUNT: {props.games.length}</p>
+        <section className="summary-grid textcol-main font-semibold">
+            <div className="count">
+                <p className="gridtext-main">{games.length}</p>
+                <p className="gridtext-title">Total Games</p>
+            </div>
+
+            <div className="tags gap-[30px]">
+                <p className="text-[25px]">Action</p>
+                <p className="text-[25px]">Adventure</p>
+                <p className="text-[25px]">RPG</p>
+                <p className="gridtext-title">Favourite Tags</p>
+            </div>
+
+            <div className="completion">
+                <p className="gridtext-main">68%</p>
+                <p className="gridtext-title">Completion Rate</p>
+            </div>
+            <div className="hours">
+                <p className="gridtext-main">1320</p>
+                <p className="gridtext-title">Total Hours</p>
+            </div>
+            <div className="comment gap-[15px]">
+                <p className="text-[20px] text-left w-full px-[15px]">Red Dead Redemption 2</p>
+                <p className="text-[16px] font-normal px-[15px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque aspernatur consequuntur, vitae adipisci maxime consequatur, voluptatibus doloremque tempora totam autem ipsum voluptates labore illo, atque veniam.</p>
+                <p className="gridtext-title">Random Comment</p>
+            </div>
+            <div className="platform">
+                <p className="text-[30px]">PlayStation 4</p>
+                <p className="gridtext-title">Popular Platform</p>
+            </div>
+            <div className="date gap-[20px]">
+                <p className="text-[30px]">20</p>
+                <p className="text-[30px]">December</p>
+                <p className="text-[30px]">2022</p>
+
+                <p className="gridtext-title">Last Game Completion</p>
+            </div>
+            <div className="rating">
+                <p className="gridtext-main">78/100</p>
+                <p className="gridtext-title">Average Rating</p>
+            </div>
+            <div className="average-time">
+                <p className="gridtext-main">120</p>
+                <p className="gridtext-title">Game Hours</p>
+            </div>
         </section>
     )
 }
