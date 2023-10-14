637b8143e38fc88273697dea991a5c1cb83a8ef3
FalejevV <v.o.v.a@hotmail.com>
2023-10-14 19:17:10 +0300

    moved cron api trigger to GET

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -2,15 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 
 export async function GET(request: NextRequest) {
-    return new NextResponse(JSON.stringify("Only post"), {
-        status: 400,
-        headers: { "Content-Type": "application/json" },
-    });
-}
-
-
-export async function POST(request: Request){
-    const res = await request.json()
+      const res = await request.json()
     console.log(res.key);
     if(res.key && res.key === process.env.CRON_KEY){
         console.log("YES");
